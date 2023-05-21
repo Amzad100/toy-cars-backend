fe676a7d6ee4bf9c32250593f072b984f1506a50
Amzad100 <mdamzad2003a@gmail.com>
2023-05-21 23:17:04 +0600

    Update added

--- index.js
+++ index.js
@@ -27,12 +27,6 @@ async function run() {
         const toysCollection = client.db('toycars').collection('toys');
 
 
-        const indexkey = { toy_name: 1, category_name: 1 };
-        const indexoption = { name: "toy_namecategory_name" };
-
-        const result = await toysCollection.createIndex(indexkey, indexoption)
-
-
         app.post('/addtoy', async (req, res) => {
             const body = req.body;
             const result = await toysCollection.insertOne(body)
@@ -44,7 +38,7 @@ async function run() {
             const result = await toysCollection.find({}).toArray();
             res.send(result);
         })
-        
+
         app.get('/alltoys/:category', async (req, res) => {
             console.log(req.params.category)
             if (req.params.category == "SportsCars" || req.params.category == "PoliceCars" || req.params.category == "Truck") {
@@ -62,8 +56,53 @@ async function run() {
             res.send(result);
         })
 
+        app.patch('/mytoys/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updatetoy = req.body;
+            console.log(updatetoy)
 
-        
+            const updatedoc = {
+                $set: {
+                    name: updatetoy.name,
+                    toy_name: updatetoy.toy_name,
+                    price: updatetoy.price,
+                    rating: updatetoy.rating,
+                    available_quantity: updatetoy.available_quantity,
+                }
+            }
+
+
+        })
+
+        app.delete('/mytoys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toysCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+
+        app.get('/toys', async (req, res) => {
+            const cursor = toysCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.get('/toys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const options = {
+                // Include only the `title` and `imdb` fields in the returned document
+                projection: { toy_name: 1, category_name: 1, picture: 1, price: 1, rating: 1, category_id: 1, available_quantity: 1 },
+            };
+
+            const result = await toysCollection.findOne(query, options)
+            res.send(result);
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
